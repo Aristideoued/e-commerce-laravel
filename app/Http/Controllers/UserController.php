8fa9f2f2f0f779810bdc73cb38be0b6a81ad6dfa
Aristide12 <ouedraogoaris@gmail.com>
2020-12-04 16:13:50 +0000

    login and register done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,11 +11,20 @@ class UserController extends Controller
     function login(Request $req){
       $user= User::where(['email'=>$req->email])->first();
       if(!$user || !Hash::check($req->password,$user->password)){
-        return "Username or password is not matched";
+        return "Username or password is not correct";
       }
       else {
         $req->session()->put('user',$user);
         return redirect('/');
       }
+    }
+    function register(Request $req){
+      //return $req->input();
+      $user=new User;
+      $user->name=$req->name;
+      $user->email=$req->email;
+      $user->password=Hash::make($req->password);
+      $user->save();
+      return redirect('/login');
     }
 }
